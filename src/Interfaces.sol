e460438d2961aae5dc2038135175a00b77bd7184
kumo7890 <100519054+kumo7890@users.noreply.github.com>
2026-06-04 13:01:08 +0100

    Update Interfaces.sol

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -3,20 +3,22 @@ pragma solidity ^0.8.20;
 
 interface IORMPPort {
     function recv(
-        uint256 fromChainId,
         address fromDapp,
         address toDapp,
         bytes calldata message
-    ) external;
+    ) external payable;
 
     function ORMP() external view returns (address);
+    function ormp() external view returns (address);
+    function peerOf(uint256 chainId) external view returns (address);
 }
 
 interface IMsgportMessager {
     function receiveMessage(
-        uint256 fromChainId,
-        address fromDapp,
-        bytes calldata message
+        uint256 _srcAppChainId,
+        address _remoteAppAddress,
+        address _localAppAddress,
+        bytes memory _message
     ) external;
 
     function remoteMessagerOf(uint256 chainId) external view returns (address);
@@ -25,4 +27,33 @@ interface IMsgportMessager {
 
 interface IORMP {
     function trusted(address) external view returns (bool);
+
+    function recv(
+        Message calldata message,
+        bytes calldata proof
+    ) external;
+}
+
+struct Message {
+    address channel;
+    uint256 index;
+    uint256 fromChainId;
+    address from;
+    uint256 toChainId;
+    address to;
+    uint256 gasLimit;
+    bytes encoded;
+}
+
+interface IXTokenIssuing {
+    function issue(
+        uint256 _remoteChainId,
+        address _originalToken,
+        address _originalSender,
+        address _recipient,
+        address _rollbackAccount,
+        uint256 _amount,
+        uint256 _nonce,
+        bytes calldata _extData
+    ) external;
 }
